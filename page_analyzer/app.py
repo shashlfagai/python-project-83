f1554a1f5cddf9bd29c517e4ff85c52b04135062
Loginov <itsmeegorloginov@gmail.com>
2024-02-14 16:33:04 +0300

    page '/' was made

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def say_hello():
+def page_analyzer():
     return render_template('index.html')
+
+
+@app.get('/urls')
+def analyzed_pages():
+    return "It's not ready"
+
+
+@app.post('/urls')
+def analyzing_page():
+    return "It's not ready"
 
 
 if __name__ == '__main__':
