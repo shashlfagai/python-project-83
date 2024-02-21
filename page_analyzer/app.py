c2397ebcd702debf5f8703f1668020796bb80d87
Loginov <itsmeegorloginov@gmail.com>
2024-02-21 17:31:01 +0300

    fixed connect and made validator url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,10 +4,11 @@ import secrets
 import psycopg2
 from datetime import datetime
 import os
+import validators
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
+# conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)
 secret_key = secrets.token_hex(32)
@@ -39,32 +40,41 @@ def analyzed_pages():
         name = request.form.get('url')
         created_at = datetime.now().date()
         cur = conn.cursor()
-        try:
-            cur.execute("SELECT COUNT(*) FROM urls WHERE name = %s", (name,))
-            count = cur.fetchone()[0]
-            if count == 0:
+        if validators.url(name):
+            try:
                 cur.execute(
-                    'INSERT INTO urls (name, created_at)\
-                        VALUES (%s, %s) RETURNING id',
-                    (name, created_at)
+                    "SELECT COUNT(*) FROM urls WHERE name = %s",
+                    (name,)
                 )
-                url_id = cur.fetchone()[0]
-                conn.commit()
-                cur.close()
-                # flash('Страница успешно добавлена', 'success')
-            else:
-                cur.execute("SELECT id FROM urls WHERE name = %s", (name,))
-                url_id = cur.fetchone()[0]
-                cur.close()
-            return redirect(url_for('showing_info', id=url_id))
-        except psycopg2.Error as e:
-            print(e)
-            # flash('Ошибка при выполнении запроса к базе данных', 'danger')
+                count = cur.fetchone()[0]
+                if count == 0:
+                    cur.execute(
+                        'INSERT INTO urls (name, created_at)\
+                            VALUES (%s, %s) RETURNING id',
+                        (name, created_at)
+                    )
+                    url_id = cur.fetchone()[0]
+                    conn.commit()
+                    cur.close()
+                    # flash('Страница успешно добавлена', 'success')
+                else:
+                    cur.execute("SELECT id FROM urls WHERE name = %s", (name,))
+                    url_id = cur.fetchone()[0]
+                    cur.close()
+                return redirect(
+                    url_for('showing_info', id=url_id)
+                )
+            except psycopg2.Error as e:
+                print(e)
+# flash('Ошибка при выполнении запроса к базе данных', 'danger')
+                return redirect(url_for('page_analyzer'))
+        else:
             return redirect(url_for('page_analyzer'))
 
 
 @app.route('/urls/<id>')
 def showing_info(id):
+    conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
     cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
     data = cur.fetchall()[0]
