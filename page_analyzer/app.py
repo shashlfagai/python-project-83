590cb5735c3fb8aa65e9e99d664cea68103623e2
Loginov <itsmeegorloginov@gmail.com>
2024-02-21 13:56:22 +0300

    trying

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -19,43 +19,44 @@ def page_analyzer():
     return render_template('index.html')
 
 
-@app.get('/urls')
+@app.route('/urls', methods=['POST', 'GET'])
 def analyzed_pages():
-    cur = conn.cursor()
-    cur.execute("SELECT id, name FROM urls ORDER BY id DESC")
-    rows = cur.fetchall()
-    table_html = ""
-    for row in rows:
-        table_html += "<tr>"
-        for column in row:
-            table_html += "<td>{}</td>".format(column)
-        table_html += "</tr>"
-    cur.close()
-    return render_template('urls.html', table=table_html)
-
-
-@app.post('/urls')
-def analyzing_page():
-    name = request.form.get('url')
-    created_at = datetime.now().date()
-    cur = conn.cursor()
-    cur.execute("SELECT COUNT(*) FROM urls WHERE name = %s", (name,))
-    count = cur.fetchone()[0]
-    if count == 0:
-        cur.execute(
-            'INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id',
-            (name, created_at)
-        )
-        url_id = cur.fetchone()[0]
-        conn.commit()
+    if request.method == 'GET':
+        cur = conn.cursor()
+        cur.execute("SELECT id, name FROM urls ORDER BY id DESC")
+        rows = cur.fetchall()
+        table_html = ""
+        for row in rows:
+            table_html += "<tr>"
+            for column in row:
+                table_html += "<td>{}</td>".format(column)
+            table_html += "</tr>"
         cur.close()
-        flash('Страница успешно добавлена', 'success')
-        return redirect(url_for('showing_info', id=url_id))
+        return render_template('urls.html', table=table_html)
     else:
-        cur.execute("SELECT id FROM urls WHERE name = %s", (name,))
-        url_id = cur.fetchone()[0]
-        cur.close()
-        return redirect(url_for('showing_info', id=url_id))
+        name = request.form.get('url')
+        created_at = datetime.now().date()
+        cur = conn.cursor()
+        try:
+            cur.execute("SELECT COUNT(*) FROM urls WHERE name = %s", (name,))
+            count = cur.fetchone()[0]
+            if count == 0:
+                cur.execute(
+                    'INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id',
+                    (name, created_at)
+                )
+                url_id = cur.fetchone()[0]
+                conn.commit()
+                cur.close()
+                # flash('Страница успешно добавлена', 'success')
+            else:
+                cur.execute("SELECT id FROM urls WHERE name = %s", (name,))
+                url_id = cur.fetchone()[0]
+                cur.close()
+            return redirect(url_for('showing_info', id=url_id))
+        except psycopg2.Error as e:
+            # flash('Ошибка при выполнении запроса к базе данных', 'danger')
+            return redirect(url_for('page_analyzer'))
 
 
 @app.route('/urls/<id>')
