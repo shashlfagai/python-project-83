0f6e3cae7f87e2d01deadfa37f1211d8cc9e8798
Loginov <itsmeegorloginov@gmail.com>
2024-02-24 23:19:16 +0300

    4th step

--- database.sql
+++ database.sql
@@ -3,3 +3,14 @@ CREATE TABLE IF NOT EXISTS urls (
     name varchar(255),
     created_at date
 );
+
+
+CREATE TABLE IF NOT EXISTS url_cheks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id varchar(255),
+    status_code varchar(255),
+    h1 varchar(255),
+    title varchar(255),
+    description varchar(255),
+    created_at date
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,7 +8,7 @@ import validators
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-# conn = psycopg2.connect(DATABASE_URL)
+conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)
 secret_key = secrets.token_hex(32)
@@ -22,21 +22,28 @@ def page_analyzer():
 
 @app.route('/urls', methods=['POST', 'GET'])
 def analyzed_pages():
-    conn = psycopg2.connect(DATABASE_URL)
+    # conn = psycopg2.connect(DATABASE_URL)
     if request.method == 'GET':
         cur = conn.cursor()
-        cur.execute("SELECT id, name FROM urls ORDER BY id DESC")
+        cur.execute("""
+    SELECT urls.id, urls.name, url_checks.created_at, url_checks.status_code
+    FROM urls
+    LEFT JOIN url_checks ON urls.id = url_checks.url_id
+    ORDER BY urls.id DESC
+""")
         rows = cur.fetchall()
         table_html = ""
         for row in rows:
-            table_html += "<tr>"
             for column in row:
-                table_html += "<td>{}</td>".format(column)
+                if column is not None:
+                    table_html += "<td>{}</td>".format(column)
+                else:
+                    table_html += "<td></td>"
             table_html += "</tr>"
         cur.close()
         return render_template('urls.html', table=table_html)
     else:
-        conn = psycopg2.connect(DATABASE_URL)
+        # conn = psycopg2.connect(DATABASE_URL)
         name = request.form.get('url')
         created_at = datetime.now().date()
         cur = conn.cursor()
@@ -72,17 +79,51 @@ def analyzed_pages():
             return redirect(url_for('page_analyzer'))
 
 
-@app.route('/urls/<id>')
+@app.route('/urls/<id>', methods=['GET', 'POST'])
 def showing_info(id):
-    conn = psycopg2.connect(DATABASE_URL)
+    if request.method == 'GET':
+        # conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
+        data = cur.fetchall()[0]
+        id = data[0]
+        url = data[1]
+        created_at = data[2]
+        cur.execute("""
+        SELECT id, status_code, h1, title, description, created_at
+        FROM url_checks
+        WHERE url_id = %s
+        ORDER BY created_at DESC
+        """, (id,))
+        rows = cur.fetchall()
+        table_html = ""
+        for row in rows:
+            for column in row:
+                if column is not None:
+                    table_html += "<td>{}</td>".format(column)
+                else:
+                    table_html += "<td></td>"
+            table_html += "</tr>"
+        cur.close()
+        return render_template(
+            'url.html', id=id, url=url, date=created_at, table_check=table_html
+        )
+    else:
+        redirect(url_for('check_url', id=id))
+
+
+@app.post('/urls/<id>/checks')
+def check_url(id):
+    created_at = datetime.now().date()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
-    data = cur.fetchall()[0]
-    id = data[0]
-    url = data[1]
-    created_at = data[2]
+    cur.execute("""
+        INSERT INTO
+        url_checks (url_id, created_at)
+        VALUES (%s, %s)
+    """, (id, created_at))
+    conn.commit()
     cur.close()
-    return render_template('url.html', id=id, url=url, date=created_at)
+    return redirect(url_for('showing_info', id=id))
 
 
 if __name__ == '__main__':
