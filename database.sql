07bb9ef7768b8e1ea7814e22207226474f7ba0ba
Loginov <itsmeegorloginov@gmail.com>
2024-02-28 18:50:04 +0300

    6th step (2)

--- database.sql
+++ database.sql
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS urls (
 );
 
 
-CREATE TABLE IF NOT EXISTS url_cheks (
+CREATE TABLE IF NOT EXISTS url_checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id varchar(255),
+    url_id bigint REFERENCES urls (id),
     status_code varchar(255),
     h1 varchar(255),
     title varchar(255),
